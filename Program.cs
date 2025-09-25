c0951fcb715cf5cf14e5471b36b89ab54fbab3ae
ProgramerPlus <hipitelo@outlook.com>
2025-09-24 22:30:13 -0300

    Exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string texto;
+
+Console.ForegroundColor = (ConsoleColor)2;
+Console.WriteLine("--------------Cebolinha--------------");
+Console.ResetColor();
+Console.WriteLine("Escreva uma frase:");
+texto = Console.ReadLine()!;
+texto = texto.Replace("rr", "l");
+texto = texto.Replace("r", "l");
+
+Console.WriteLine(texto);
